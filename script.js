64c2c9ffb02717a7d9b5a56b08fde7b2a942e3dc
Amit DevOps <asemwal125@gmail.com>
2025-01-25 00:05:43 +0530

    Visitor count

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.querySelectorAll('.sidebar a').forEach(link => {
         document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+const ws = new WebSocket('wss://7hv68471za.execute-api.ap-south-1.amazonaws.com/production/');
+
+ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    document.getElementById('visitorCount').innerText = data.VisitorCount;
+};
+
+ws.onopen = () => console.log('WebSocket connection established.');
+ws.onerror = (err) => console.error('WebSocket error:', err);
+ws.onclose = () => console.log('WebSocket connection closed.');
